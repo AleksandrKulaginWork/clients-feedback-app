473790143ee84235935d5285b60cc0eaba49b507
AleksandrKulaginWork <aleksandr.kulagin.work.email@gmail.com>
2023-08-09 16:54:40 +0300

    Added more CSS styles

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -11,33 +11,37 @@ export function Form({
 }) {
 	return (
 		<div className={style.sectioForm}>
-			<div className={style.header}>
-				<img src={iconHeader} alt="icon-header" />
-				<h2>Свяжитесь с нами</h2>
+			<div className={style.bodyForm}>
+				<div className={style.header}>
+					<img src={iconHeader} alt="icon-header" />
+					<h1>Свяжитесь с нами</h1>
+				</div>
+
+				<p className={style.announcement}>Отправьте нам сообщение и мы ответим в ближайшее время</p>
+
+				<form onSubmit={handleSubmit} className={style.form}>
+					<div className={style.blockInput}>
+						<input value={username} placeholder="Ваше имя*" type="text" name="username" required />
+						<input value={telephone} placeholder="Телефон" type="tel" name="telephone" />
+						<input value={password} placeholder="Электронная почта*" type="password" name="password" required />
+						<textarea value={message} placeholder="Текст сообщения*" />
+					</div>
+					<div className={style.description}>*обязательные поля</div>
+					<label className={style.checkbox} htmlFor="myCheckbox">
+						<input type="checkbox" id="myCheckbox" name="myCheckbox" />
+						<p>
+							Я согласен(-на) с
+							<span> правилами </span>
+							о обработке моих персональных данных
+						</p>
+					</label>
+
+					<button className={style.button} type="submit">
+						<img src={iconButton} alt="iconButton" />
+						<span>Отправить сообщение</span>
+					</button>
+				</form>
 			</div>
-
-			<p className={style.description}>Отправьте нам сообщение и мы ответим в ближайшее время</p>
-
-			<form onSubmit={handleSubmit} className={style.form}>
-				<input value={username} placeholder="Ваше имя*" type="text" name="username" required />
-				<input value={telephone} placeholder="Телефон" type="tel" name="telephone" />
-				<input value={password} placeholder="Электронная почта*" type="password" name="password" required />
-				<textarea value={message} placeholder="Текст сообщения*" />
-				<div className={style.description}>*обязательные поля</div>
-				<label className={style.checkbox} htmlFor="myCheckbox">
-					<input type="checkbox" id="myCheckbox" name="myCheckbox" />
-					<p>
-						Я согласен(-на) с
-						<span> правилами </span>
-						о обработке моих персональных данных
-					</p>
-				</label>
-
-				<button className={style.button} type="submit">
-					<img src={iconButton} alt="iconButton" />
-					<span>Отправить сообщение</span>
-				</button>
-			</form>
 		</div>
 	);
 }
--- src/components/FormSection/index.jsx
+++ src/components/FormSection/index.jsx
@@ -1,5 +1,6 @@
 import style from './style.module.css';
 import imageChildren from '../../assets/image-form/image-children.png';
+import Circle from '../../assets/image-form/Group 882.png';
 import { Form } from '../Form';
 
 export function FormSection() {
@@ -7,6 +8,7 @@ export function FormSection() {
 		<div className={style.formSection}>
 			<div className={style.sectionImage}>
 				<img src={imageChildren} alt="children" />
+				<img className={style.circle} src={Circle} alt="Fill the form" />
 			</div>
 			<Form />
 		</div>
